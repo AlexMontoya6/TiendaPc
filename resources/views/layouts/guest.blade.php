ff56c01d3130f661b56349225f6379e70fb67943
Alejandro Montoya <alexmonty6@gmail.com>
2025-02-13 22:20:15 +0100

    principio nav bar

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,7 +11,54 @@
 </head>
 <body class="bg-gray-100">
     <!-- Barra de navegación -->
-    @livewire('navigation-menu')
+    <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div class="flex justify-between h-16">
+                <div class="flex">
+                    <!-- Logo -->
+                    <div class="shrink-0 flex items-center">
+                        <a href="{{ route('home') }}">
+                            <x-application-mark class="block h-9 w-auto" />
+                        </a>
+                    </div>
+
+                    <!-- Navigation Links -->
+                    <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                        <x-nav-link href="{{ route('home') }}" :active="request()->routeIs('home')">
+                            {{ __('Inicio') }}
+                        </x-nav-link>
+                        <x-nav-link href="">
+                            {{ __('Productos') }}
+                        </x-nav-link>
+                        <x-nav-link href="" :active="request()->routeIs('contact')">
+                            {{ __('Contacto') }}
+                        </x-nav-link>
+                    </div>
+                </div>
+
+                <!-- Authentication Dropdown -->
+                <div class="hidden sm:flex sm:items-center sm:ms-6">
+                    <div class="relative">
+                        <button @click="open = !open" class="flex items-center text-sm font-medium text-gray-700 hover:text-gray-900 focus:outline-none">
+                            {{ __('Cuenta') }}
+                            <svg class="ms-1 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                            </svg>
+                        </button>
+
+                        <div x-show="open" @click.away="open = false" class="absolute right-0 mt-2 w-48 bg-white border rounded-md shadow-lg py-1">
+                            <a href="{{ route('login') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+                                {{ __('Ingresar') }}
+                            </a>
+                            <a href="{{ route('register') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+                                {{ __('Registrarse') }}
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
 
 
 
