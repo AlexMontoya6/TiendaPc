4ee19a17d85d48fbe1cce17744afcc5636aecafd
Alejandro Montoya <alexmonty6@gmail.com>
2025-02-13 21:28:11 +0100

    empezando

---
+++ resources/views/layouts/guest.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Estilos -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @livewireStyles
+</head>
+<body class="bg-gray-100">
+    <!-- Barra de navegación -->
+    <x-navigation.navigation />
+
+
+
+    <!-- Contenido principal -->
+    <div class="min-h-screen flex flex-col items-center pt-6 sm:pt-0">
+        {{ $slot }}
+    </div>
+
+    <!-- Scripts -->
+    @livewireScripts
+</body>
+</html>
