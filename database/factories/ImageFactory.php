7b9fe0609dc007c92bd36006298ab56af8f8d68c
Alejandro Montoya <alexmonty6@gmail.com>
2025-02-14 11:34:25 +0100

    imagenes arregladas

--- database/factories/ImageFactory.php
+++ database/factories/ImageFactory.php
@@ -12,30 +12,8 @@ class ImageFactory extends Factory
 
     public function definition(): array
     {
-        // Seleccionamos el primer producto encontrado en la base de datos
-        $product = Product::first();
-
-        // Si no hay productos en la base de datos, lanzamos una excepción
-        if (!$product) {
-            throw new \Exception("No hay productos disponibles en la base de datos.");
-        }
-
-        // Obtener el siguiente número de orden para el producto
-        // Esto garantiza que el siguiente `order` sea único para cada nueva imagen creada
-        $existingOrders = Image::where('product_id', $product->id)->pluck('order')->toArray();
-
-        // Generamos el siguiente `order` que no se repita, comenzando desde 1
-        $order = 1;
-
-        // Mientras el `order` ya esté en uso, incrementamos el valor
-        while (in_array($order, $existingOrders)) {
-            $order++;
-        }
-
         return [
-            'product_id' => $product->id,  // Usamos el id del primer producto encontrado
             'path' => 'products/' . $this->faker->image('storage/app/public/products', 640, 480, null, false),  // Genera una ruta de imagen
-            'order' => $order,  // Asignamos el siguiente número de orden único
         ];
     }
 }
--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -12,26 +12,23 @@ class ProductSeeder extends Seeder
 {
     public function run()
     {
-        // Obtener los tres primeros tipos de productos
-        $productTypes = ProductType::limit(3)->get(); // Limitamos a los tres primeros tipos
+        $productTypes = ProductType::limit(3)->get();
 
         foreach ($productTypes as $productType) {
-            // Obtener las categorías asociadas a este tipo de producto
             $categories = Category::where('product_type_id', $productType->id)->get();
 
             foreach ($categories as $category) {
-                // Crear productos para cada categoría asociada al tipo de producto
                 $product = Product::factory()->create([
                     'product_type_id' => $productType->id,
                     'category_id' => $category->id,
                 ]);
 
-                // Generar entre 1 y 3 imágenes para el producto
-                $imagesCount = rand(1, 3); // Generamos entre 1 y 3 imágenes
+                $imagesCount = rand(1, 3);
 
                 for ($i = 0; $i < $imagesCount; $i++) {
                     Image::factory()->create([
                         'product_id' => $product->id,
+                        'order' => $i,
                     ]);
                 }
             }
