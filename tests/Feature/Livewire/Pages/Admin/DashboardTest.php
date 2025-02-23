22f023505bab82f507698172ae73df9a69b460bb
Alejandro Montoya <alexmonty6@gmail.com>
2025-02-23 10:11:14 +0100

    se olvido borrar esa clase antes...

---
+++ tests/Feature/Livewire/Pages/Admin/DashboardTest.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Livewire\Pages\Admin\Dashboard;
+use Livewire\Livewire;
+
+
+it('se renderiza correctamente', function () {
+    Livewire::test(Dashboard::class)
+        ->assertStatus(200);
+});
