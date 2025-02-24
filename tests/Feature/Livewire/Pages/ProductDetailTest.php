c4a87c7c4c3d82b002806d5b2dccc2f723051194
Alejandro Montoya <alexmonty6@gmail.com>
2025-02-24 02:14:59 +0100

    a

---
+++ tests/Feature/Livewire/Pages/ProductDetailTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use Livewire\Livewire;
+use App\Livewire\Pages\ProductDetail;
+use Tests\Traits\CreatesProducts;
+use Gloudemans\Shoppingcart\Facades\Cart;
+
+uses(CreatesProducts::class);
+
+it('carga el producto correctamente', function () {
+    $product = $this->newProduct();
+
+    Livewire::test(ProductDetail::class, ['product' => $product])
+        ->assertSet('product', $product)
+        ->assertSee($product->name)
+        ->assertSee(strval($product->formatted_price)); // 🔥 Usamos `formatted_price`
+});
+
+it('carga las imágenes del producto', function () {
+    $product = $this->newProduct();
+
+    // Asegurarnos de que el producto tiene imágenes
+    expect($product->images)->not()->toBeEmpty();
+
+    Livewire::test(ProductDetail::class, ['product' => $product])
+        ->assertSet('images', $product->images)
+        ->assertSee($product->images->first()->path); // Verificamos que muestra una imagen
+});
+
+it('renderiza la vista correcta', function () {
+    $product = $this->newProduct();
+
+    Livewire::test(ProductDetail::class, ['product' => $product])
+        ->assertViewIs('livewire.pages.product-detail');
+});
+
